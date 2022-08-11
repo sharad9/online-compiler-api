7556f3287b4196fc16cd64d0e691c92f8ad89929
Sharad Rakesh Maddheshiya <sharadrakeshm@gmail.com>
2022-08-11 22:01:28 +0530

    first commit

---
+++ executeJava.js
@@ -0,0 +1,21 @@
+const { c, cpp, node, python, java } = require('compile-run');
+
+
+const executeJava = async (filepath) => {
+	console.log(filepath);
+	return new Promise((resolve, reject) => {
+	
+		java.runFile(filepath, (error, result) => {
+				error && reject({ error, error });
+				result.stderr && reject(result.stderr);
+				resolve(result.stdout);
+			}
+		)
+		
+	});
+	
+};
+
+module.exports = {
+	executeJava,
+};
